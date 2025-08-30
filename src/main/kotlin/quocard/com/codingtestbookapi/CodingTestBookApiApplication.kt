6648f600e2bb11678aa668058235778583ea3a04
Sina-TehraniFard <tf.sina.system@gmail.com>
2025-08-30 23:54:31 +0900

    初回コミット

---
+++ src/main/kotlin/quocard/com/codingtestbookapi/CodingTestBookApiApplication.kt
@@ -0,0 +1,11 @@
+package quocard.com.codingtestbookapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodingTestBookApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodingTestBookApiApplication>(*args)
+}
---
+++ src/test/kotlin/quocard/com/codingtestbookapi/CodingTestBookApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package quocard.com.codingtestbookapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodingTestBookApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
